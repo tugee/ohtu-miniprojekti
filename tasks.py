98f1de2ffbb9eca1c50c55ccaa25441e7f7f5673
Tuukka Tuomikoski <tuukka.tuomikoski@gmail.com>
2021-12-01 13:32:25 +0200

    Added invoke, coverage

---
+++ tasks.py
@@ -0,0 +1,18 @@
+from invoke import task
+
+
+@task
+def start(ctx):
+    ctx.run("py src/index.py")
+
+@task
+def test(ctx):
+    ctx.run("pytest src")
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest")
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html")
