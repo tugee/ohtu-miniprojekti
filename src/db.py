5171b8ba3e978691ed2a1ec48781e45052b1d643
Maaria Wahlström <65168298+maariaw@users.noreply.github.com>
2021-12-01 19:16:55 +0200

    Korjaa (toivottavasti) herokun yhteensopimattomuus tietyn SQLAlchemyn configin kanssa

--- src/db.py
+++ src/db.py
@@ -2,5 +2,8 @@ from app import app
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+uri = getenv("DATABASE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+app.config["SQLALCHEMY_DATABASE_URI"] = uri
 db = SQLAlchemy(app)
