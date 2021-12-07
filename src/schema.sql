6e04619e45e03f568aa576707e6a7d6daa412efd
Santeri Salonen <santerisa.salonen@gmail.com>
2021-12-07 13:01:15 +0200

    Uusi lukuvinkki, schema ja template

--- src/schema.sql
+++ src/schema.sql
@@ -3,3 +3,10 @@ CREATE TABLE kayttajat (
     tunnus TEXT UNIQUE,
     salasana TEXT
 );
+
+CREATE TABLE lukuvinkit (
+    id SERIAL PRIMARY KEY,
+    nimi TEXT,
+    url VARCHAR(2083),
+    kayttaja_id INTEGER references kayttajat(id)
+);
