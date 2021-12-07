b81a4d5a1f281e3818c8c7ed68c77da6b7f7e537
Maaria Wahlström <65168298+maariaw@users.noreply.github.com>
2021-12-07 18:11:11 +0200

    Laita CI testaamaan vain läpi menevä robot test

--- run_robot_tests.sh
+++ run_robot_tests.sh
@@ -10,7 +10,8 @@ while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:5000/ping)" != "2
 done
 
 # suoritetaan testit
-poetry run robot src/tests
+poetry run robot src/tests/index.robot
+# poetry run robot src/tests
 
 # pysäytetään Flask-palvelin portissa 5000
 function clean_up {
