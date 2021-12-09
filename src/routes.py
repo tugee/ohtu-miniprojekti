ab2fd176a2c4f4dc83003e58d345aba38a2746f3
topias <topias.tallberg@gmail.com>
2021-12-09 23:52:43 +0200

    delete hide version 1.001

--- src/routes.py
+++ src/routes.py
@@ -38,12 +38,12 @@ def login():
         session["username"] = username
     return redirect("/")
 
-@app.route("/delete/<vinkki_id>", methods=["DELETE"])
+@app.route("/delete/<vinkki_id>")
 def delete(vinkki_id):
     vinkki_service.delete_vinkki(vinkki_id)
     return redirect("/")
 
-app.route("/hide/<vinkki_id>", methods=["PUT"])
+app.route("/hide/<vinkki_id>")
 def hide(vinkki_id):
     vinkki_service.hide_vinkki(vinkki_id)
     return redirect("/")
