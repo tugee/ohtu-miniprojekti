ae2fd707e7dd8e91f5fefbb6d0033bc5ff58af4e
Maaria Wahlström <65168298+maariaw@users.noreply.github.com>
2021-12-14 21:52:55 +0200

    Lisää vinkkirepositorioon metodit luettu-statuksen vaihtamiseen

--- src/repositories/vinkki_repository.py
+++ src/repositories/vinkki_repository.py
@@ -22,11 +22,25 @@ class VinkkiRepository:
         sql = text("DELETE FROM lukuvinkit WHERE nimi=:nimi")
         db.session.execute(sql, {"nimi":nimi})
         db.session.commit()
-    
+
     def hide(self, nimi):
         sql = text("UPDATE lukuvinkit piilotettu=true WHERE nimi=:nimi")
         db.session.execute(sql, {"nimi":nimi})
         db.session.commit()
-    
+
+    def set_read(self, id):
+        sql = text("UPDATE lukuvinkit SET luettu=true WHERE id=:id")
+        db.session.execute(sql, {"id":id})
+        db.session.commit()
+
+    def set_read_date(self, id):
+        sql = text("UPDATE lukuvinkit SET paivays=NOW() WHERE id=:id")
+        db.session.execute(sql, {"id":id})
+        db.session.commit()
+
+    def set_unread(self, id):
+        sql = text("UPDATE lukuvinkit SET luettu=false WHERE id=:id")
+        db.session.execute(sql, {"id":id})
+        db.session.commit()
 
 vinkki_repository = VinkkiRepository()
