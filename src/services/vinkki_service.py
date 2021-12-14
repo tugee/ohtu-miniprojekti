66c9a40e5edd93eb8ffdc19ccb705e0e7567a1b2
Maaria Wahlström <65168298+maariaw@users.noreply.github.com>
2021-12-14 21:53:57 +0200

    Lisää vinkkipalveluun metodit luettu-statuksen vaihtoon

--- src/services/vinkki_service.py
+++ src/services/vinkki_service.py
@@ -15,8 +15,15 @@ class VinkkiService:
 
     def delete_vinkki(self, nimi):
         self._vinkki_repository.delete(nimi)
-    
+
     def hide_vinkki(self, nimi):
         self._vinkki_repository.hide(nimi)
+
+    def set_read_vinkki(self, id):
+        self._vinkki_repository.set_read(id)
+        self._vinkki_repository.set_read_date(id)
+
+    def set_unread_vinkki(self, id):
+        self._vinkki_repository.set_unread(id)
 
 vinkki_service = VinkkiService()
