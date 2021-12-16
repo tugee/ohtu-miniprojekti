bd7931bf94798eb960ae86dd43b6b3e0e2958a08
ilkkaluu <xilehx@gmail.com>
2021-12-16 23:41:11 +0200

    user service testejä

--- src/tests/services/user_service_test.py
+++ src/tests/services/user_service_test.py
@@ -1,25 +1,34 @@
 import unittest
+from unittest.mock import Mock
+from werkzeug.security import generate_password_hash
+#from app import db
 from services.user_service import UserService
 
-class FakeUserRepository:
-    def __init__(self):
-        self.users = {}
-    
-    def find_by_username(self, username):
-        return self.users[username]
-    
-    def create(self, username, password_hash):
-        if username in self.users.keys():
-            return
-        self.users[username] = password_hash
+class TestUserServiceClass(unittest.TestCase):
+    def setUp(self) -> None:
+        self.user_repo_mock = Mock()
+        self.user_model_mock = Mock()
 
-class TestUserService(unittest.TestCase):
-    def setUp(self):
-        pass
+        self.user_repo_mock.find_by_username.side_effect = [
+            None,
+            ('kayttaja', generate_password_hash('salasana', method='sha256'))
+        ]
 
-    def test_luotu_kayttaja_menee_tietokantaan(self):
-        fake_repo = FakeUserRepository()
-        service = UserService(fake_repo)
-        service.create_user("kayttaja", "salasana")
-        self.assertTrue(fake_repo.users["kayttaja"])
-        
+        self.user_service = UserService(self.user_repo_mock)
+
+    def test_check_credentials_checks_for_username(self):
+        self.user_service.check_credentials('kayttaja', 'salasana')
+        self.user_repo_mock.find_by_username.assert_called_with('kayttaja')
+
+    def test_check_credentials_returns_none_if_not_user(self):
+        result = self.user_service.check_credentials('kayttaja', 'salasana')
+        self.assertIsNone(result)
+
+    def test_check_credentials_returns_valid_user(self):
+        self.user_service.check_credentials('kayttaja', 'salasana')
+        result = self.user_service.check_credentials('kayttaja', 'salasana')
+        self.assertIsNotNone(result)
+
+    def test_create_user_works(self):
+        self.user_service.create_user("kayttaja", "salasana")
+        self.user_repo_mock.create.assert_called()        
