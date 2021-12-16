019c41997d69606252495cbf9a6cd70a4b4ff5eb
Maaria Wahlström <65168298+maariaw@users.noreply.github.com>
2021-12-16 02:10:50 +0200

    Fiksaa import-rinkula (mahdollisesti)

--- src/app.py
+++ src/app.py
@@ -1,6 +1,19 @@
 from flask import Flask
+from os import getenv
+from db import db
 
-app = Flask(__name__)
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+def create_app():
+    app = Flask(__name__)
+    app.secret_key = getenv("SECRET_KEY")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    uri = getenv("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
+    db.init_app(app)
+    return app
+
+app = create_app()
+app.run
 
 import routes
